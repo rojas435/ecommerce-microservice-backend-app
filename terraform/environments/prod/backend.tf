e090bc2bfe730410421945c1976cb75c89f9858f
Felipe Rojas <pradofeliperojas@gmail.com>
2025-11-19 12:45:14 -0500

    feat: configure Terraform remote backend in Azure Storage Account

---
+++ terraform/environments/prod/backend.tf
@@ -0,0 +1,11 @@
+# Backend configuration for Terraform state
+# Stores state in Azure Storage Account for remote access, locking, and versioning
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstaterojas435"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
